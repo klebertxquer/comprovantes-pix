810fdadbad891a9831c3475636ddac03c0d773bb
klebertxquer <kleber.b4nk@gmail.com>
2025-06-19 00:48:12 -0300

    app.py

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ def extrair_valor(texto, chave, pos=1):
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
 
