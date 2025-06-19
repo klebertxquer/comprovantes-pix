b858edadf7134a1403b022deb3f7ca5e1729f3e9
klebertxquer <kleber.b4nk@gmail.com>
2025-06-19 00:35:50 -0300

    app.py

--- app.py
+++ app.py
@@ -52,4 +52,7 @@ def extrair_valor(texto, chave, pos=1):
     return linhas[pos - 1].replace(chave, "").strip()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
